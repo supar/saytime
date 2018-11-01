7e996e201f46e7d749ca9d9a8b4cac404e03f3eb
supar <paulrez@gmail.com>
2018-11-01 12:03:24 +0300

    Разбор формата YYMMDD.hhmmss

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"strconv"
+	"time"
+)
+
+type Time struct {
+	time.Time
+}
+
+const (
+	DateFloatFormat = "060102.150405"
+)
+
+var (
+	ErrDateFormat = errors.New("unexpected date foemat YYMMDD")
+)
+
+func main() {
+
+}
+
+func ParseFloated(f []byte) (t Time, err error) {
+	var (
+		miliPart []byte
+
+		timePart = bytes.Repeat([]byte("0"), 6)
+		parts    = bytes.Split(f, []byte("."))
+	)
+
+	switch l := len(parts); {
+	case l == 0 || l > 2:
+		return t, ErrDateFormat
+
+	default:
+		if len(parts[0]) != 6 {
+			return t, ErrDateFormat
+		}
+
+		if len(parts) > 1 {
+			if len(parts[1]) > 6 {
+				miliPart = append(miliPart, parts[1][6:]...)
+
+				parts[1] = parts[1][:6]
+			}
+
+			copy(timePart, parts[1])
+			parts[1] = timePart
+		} else {
+			parts = append(parts, timePart)
+		}
+	}
+
+	t.Time, err = time.Parse(DateFloatFormat, string(bytes.Join(parts, []byte("."))))
+
+	if len(miliPart) > 0 && err == nil {
+		var msec int64
+
+		if msec, err = strconv.ParseInt(string(miliPart), 10, 64); err == nil {
+			t.Time = t.Time.Add(time.Millisecond * time.Duration(msec))
+		}
+	}
+
+	return
+}
---
+++ time_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func Test_DateInputOverflowDetect(t *testing.T) {
+	data := []string{
+		"",
+		".",
+		"..",
+		"1",
+		"12",
+		"122",
+		"1220",
+		"11220",
+		"0181220",
+	}
+
+	for i, l := 0, len(data); i < l; i++ {
+		_, err := ParseFloated([]byte(data[i]))
+
+		if err != ErrDateFormat {
+			t.Error("expected overflow error")
+		}
+	}
+}
+
+func Test_DateTimeInput(t *testing.T) {
+	data := []struct {
+		in, out string
+	}{
+		{"180213", "180213.000000"},
+		{"180213.12", "180213.120000"},
+		{"180213.081833", "180213.081833"},
+		{"180313.2218332", "180313.221833"},
+		{"180313.221800042", "180313.221800"},
+	}
+
+	for i, l := 0, len(data); i < l; i++ {
+		tm, err := ParseFloated([]byte(data[i].in))
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if f := tm.Format(DateFloatFormat); data[i].out != f {
+			t.Error("not equal: " + data[i].out + " <=> " + f)
+		}
+
+		if len(data[i].in) > 13 {
+			nsec := tm.Nanosecond()
+			msec, _ := strconv.Atoi(data[i].in[13:])
+
+			if nsec != (int(time.Millisecond) * msec) {
+				t.Error("invalid miliseconds value")
+			}
+		}
+	}
+}
