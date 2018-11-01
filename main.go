27ee2b0fe030910aa54f69834246dac71d734143
supar <paulrez@gmail.com>
2018-11-01 17:14:00 +0300

    Add delta parse

--- main.go
+++ main.go
@@ -11,12 +11,17 @@ type Time struct {
 	time.Time
 }
 
+type Delta struct {
+	time.Duration
+	Year, Month, Day int
+}
+
 const (
 	DateFloatFormat = "060102.150405"
 )
 
 var (
-	ErrDateFormat = errors.New("unexpected date foemat YYMMDD")
+	ErrDateFormat = errors.New("unexpected date format YYMMDD")
 )
 
 func main() {
@@ -24,11 +29,16 @@ func main() {
 }
 
 func (tm *Time) UnmarshalJSON(data []byte) (err error) {
-	(*tm), err = ParseFloated(data)
+	(*tm), err = parseDateTime(data)
 	return
 }
 
-func ParseFloated(f []byte) (t Time, err error) {
+func (dl *Delta) UnmarshalJSON(data []byte) (err error) {
+	(*dl), err = parseDelta(data)
+	return
+}
+
+func parseDateTime(f []byte) (t Time, err error) {
 	var (
 		miliPart []byte
 
@@ -70,4 +80,90 @@ func ParseFloated(f []byte) (t Time, err error) {
 	}
 
 	return
+}
+
+func parseDelta(data []byte) (dur Delta, err error) {
+	var (
+		parts = bytes.Split(data, []byte("."))
+	)
+
+	if l := len(parts); l == 0 || l > 2 {
+		return dur, ErrDateFormat
+	}
+
+	// YYMMDD
+	err = each(parts[0], func(idx, n int) error {
+		switch idx {
+		case 1:
+			dur.Year = n
+		case 2:
+			dur.Month = n
+		case 3:
+			dur.Day = n
+		}
+		return nil
+	})
+
+	if err != nil {
+		return
+	}
+
+	// hhmmss
+	if len(parts) > 1 {
+		switch l := len(parts[1]); {
+		// tie short
+		case l < 6:
+			tmCopy := bytes.Repeat([]byte("0"), 6)
+			copy(tmCopy, parts[1][0:])
+			parts[1] = tmCopy
+
+		// cut long
+		case l > 6:
+			var msec int
+			if msec, err = strconv.Atoi(string(parts[1][6:])); err != nil {
+				return
+			}
+
+			dur.Duration = dur.Duration + time.Millisecond*time.Duration(msec)
+			parts[1] = parts[1][:6]
+		}
+
+		err = each(parts[1], func(idx, n int) error {
+			var x time.Duration
+
+			switch idx {
+			case 1:
+				x = time.Hour * time.Duration(n)
+			case 2:
+				x = time.Minute * time.Duration(n)
+			case 3:
+				x = time.Second * time.Duration(n)
+			}
+
+			dur.Duration = dur.Duration + x
+
+			return nil
+		})
+	}
+
+	return
+}
+
+func each(dt []byte, fn func(int, int) error) error {
+	num, err := strconv.Atoi(string(dt))
+	if err != nil {
+		return err
+	}
+
+	for i, l, s := 10000, 0, 1; i > l; i, s = int(i/100), s+1 {
+		y := int(num / i)
+
+		if err := fn(s, y); err != nil {
+			return err
+		}
+
+		num = num - y*i
+	}
+
+	return nil
 }
--- time_test.go
+++ time_test.go
@@ -21,7 +21,7 @@ func Test_DateInputOverflowDetect(t *testing.T) {
 	}
 
 	for i, l := 0, len(data); i < l; i++ {
-		_, err := ParseFloated([]byte(data[i]))
+		_, err := parseDateTime([]byte(data[i]))
 
 		if err != ErrDateFormat {
 			t.Error("expected overflow error")
@@ -41,7 +41,7 @@ func Test_DateTimeInput(t *testing.T) {
 	}
 
 	for i, l := 0, len(data); i < l; i++ {
-		tm, err := ParseFloated([]byte(data[i].in))
+		tm, err := parseDateTime([]byte(data[i].in))
 
 		if err != nil {
 			t.Error(err)
@@ -85,6 +85,61 @@ func Test_UnmmarshalJSONDateTime(t *testing.T) {
 
 		if f := dt.DateTime.Format(DateFloatFormat); f != data[i].out {
 			t.Error("not equal: " + data[i].out + " <=> " + f)
+		}
+	}
+}
+
+func Test_parseDelta(t *testing.T) {
+	data := []struct {
+		in  string
+		dlt Delta
+	}{
+		{"180213", Delta{Duration: 0, Year: 18, Month: 2, Day: 13}},
+		{"180213.000001", Delta{Duration: 1 * time.Second, Year: 18, Month: 2, Day: 13}},
+		{"180213.100001", Delta{Duration: 10*time.Hour + 1*time.Second, Year: 18, Month: 2, Day: 13}},
+		{"180213.1310", Delta{Duration: 13*time.Hour + 10*time.Minute, Year: 18, Month: 2, Day: 13}},
+		{"180213.13", Delta{Duration: 13 * time.Hour, Year: 18, Month: 2, Day: 13}},
+		{"180213.0000004", Delta{Duration: 4 * time.Millisecond, Year: 18, Month: 2, Day: 13}},
+		{"180213.00000033", Delta{Duration: 33 * time.Millisecond, Year: 18, Month: 2, Day: 13}},
+	}
+
+	for i, l := 0, len(data); i < l; i++ {
+		dt, err := parseDelta([]byte(data[i].in))
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if dt != data[i].dlt {
+			t.Errorf("not equal %#v <=> %#v\n", data[i].dlt, dt)
+		}
+	}
+}
+
+func Test_UnmmarshalJSONDelta(t *testing.T) {
+	type tm struct {
+		Delta Delta `json:"delta"`
+	}
+
+	data := []struct {
+		in  string
+		dlt Delta
+	}{
+		{`{"delta":180213}`, Delta{Duration: 0, Year: 18, Month: 2, Day: 13}},
+		{`{"delta":180213.13}`, Delta{Duration: time.Hour * 13, Year: 18, Month: 2, Day: 13}},
+	}
+
+	for i, l := 0, len(data); i < l; i++ {
+		dt := tm{}
+
+		err := json.Unmarshal([]byte(data[i].in), &dt)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if dt.Delta != data[i].dlt {
+			t.Errorf("not equal %#v <=> %#v\n", data[i].dlt, dt)
 		}
 	}
 }
